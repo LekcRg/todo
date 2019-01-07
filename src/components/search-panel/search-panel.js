f813add78c9ae07ba65d960ea6a95d496a7ecf15
Alexey Romanov <lekcrg@ya.ru>
2019-01-07 23:26:39 +0300

    test

--- src/components/search-panel/search-panel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SearchPanel = () => {
-  return (
-    <input className="search-panel" placeholder="Search" type="search" />
-  );
-};
-
-export default SearchPanel;
--- src/components/todo-list-item/todo-list-item.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TodoListItem = ({ label, important = false }) => {
-  const className = `todo-item__text${important ? ' important' : ''}`
-
-  return <span className={className}>{label}</span>;
-}
-
-export default TodoListItem;
