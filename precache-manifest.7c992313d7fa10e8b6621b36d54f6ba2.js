b2fbebde7b86be3d1351c2ef0a9011626b5f703a
Alexey Romanov <lekcrg@ya.ru>
2019-01-07 23:33:57 +0300

    change homepage for gh-pages

---
+++ precache-manifest.7c992313d7fa10e8b6621b36d54f6ba2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "0188899cd3ec92cdcaa4c97ca0c75247",
+    "url": "/todo/static/media/ProximaNova-Light.0188899c.ttf"
+  },
+  {
+    "revision": "91b672c5c38a46a57d82",
+    "url": "/todo/static/css/main.8b3f8cc6.chunk.css"
+  },
+  {
+    "revision": "f61c222533e03e4114e1",
+    "url": "/todo/static/js/1.f61c2225.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/todo/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/todo/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/todo/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "d778218590f14376c366c515d384808e",
+    "url": "/todo/static/media/ProximaNova-Light.d7782185.eot"
+  },
+  {
+    "revision": "eb74a591665045d87eabfb2dc759be0a",
+    "url": "/todo/static/media/ProximaNova-Light.eb74a591.woff"
+  },
+  {
+    "revision": "91b672c5c38a46a57d82",
+    "url": "/todo/static/js/main.91b672c5.chunk.js"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/todo/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/todo/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/todo/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "18992fbc284bf8aa57a853d8a11736fd",
+    "url": "/todo/static/media/trash-red.18992fbc.svg"
+  },
+  {
+    "revision": "ae48a4645b83df967f119b4473e89105",
+    "url": "/todo/static/media/star.ae48a464.svg"
+  },
+  {
+    "revision": "47c997ddce4665cd9719cc172883a66e",
+    "url": "/todo/static/media/trash.47c997dd.svg"
+  },
+  {
+    "revision": "d685cca47cd028f30197a7e122d45f04",
+    "url": "/todo/static/media/star-red.d685cca4.svg"
+  },
+  {
+    "revision": "3195346336cce080007eb434016082db",
+    "url": "/todo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09d55d118334354fcad006bcd685eadf.js"
+  "/todo/precache-manifest.7c992313d7fa10e8b6621b36d54f6ba2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/todo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
